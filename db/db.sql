661402a4db56c3dc647eaedb28f5c000f98dfacb
Tigri <leonard.imbert@clever-cloud.com>
2016-05-12 12:11:13 +0200

    Fix naming table sql
    Signed-off-by: Tigri <leonard.imbert@clever-cloud.com>

--- db/db.sql
+++ db/db.sql
@@ -1,18 +1,18 @@
-create table user (
-    user_id uuid primary key,
+create table me_user (
+    me_user_id uuid primary key,
     username text not null,
     password text not null,
     localisation text not null,
     mail text not null
 );
 
-create table message (
-    message_id uuid primary key,
+create table me_message (
+    me_message_id uuid primary key,
     time timestamp with time zone default now() not null,
     content text not null,
-    user_id uuid not null
+    me_user_id uuid not null
 );
 
 
 alter table only 
-message add constraint user_user_id_fkey foreign key (user_id) references user(user_id);
+me_message add constraint me_user_me_user_id_fkey foreign key (me_user_id) references me_user(me_user_id);
