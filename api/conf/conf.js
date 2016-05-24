4d0ccd4e95f7a3339abf464fe8ca6b10f27e1d03
Tigri <leonard.imbert@clever-cloud.com>
2016-05-24 11:15:38 +0200

    Add API_PORT to the required environement variables
    Signed-off-by: Tigri <leonard.imbert@clever-cloud.com>

--- api/conf/conf.js
+++ api/conf/conf.js
@@ -3,14 +3,17 @@ var PG_USER = process.env.PG_USER;
 var PG_URL = process.env.PG_URL;
 var PG_PORT = process.env.PG_PORT;
 var PG_DATABASE = process.env.PG_DATABASE;
+var API_PORT = process.env.API_PORT;
 
 if(
-  PG_URL === undefined || 
+  (PG_URL === undefined ||
   (PG_PASSWORD === undefined &&
   PG_USER === undefined &&
   PG_PORT === undefined &&
-  PG_DATABASE === undefined)
+  PG_DATABASE === undefined)) &&
+  API_PORT === undefined
 ) {
+  console.error(process.env);
   throw 'Cannot retrieve environement variables';
 }
 
@@ -22,5 +25,6 @@ module.exports = {
   PG_URL,
   PG_PORT,
   PG_DATABASE,
-  CONNECTION_URL
+  CONNECTION_URL,
+  API_PORT
 }
