4ed71689296534668fc605135e93136212f11a06
Tigri <leonard.imbert@clever-cloud.com>
2016-05-25 11:27:10 +0200

    Update the database scheme
    Signed-off-by: Tigri <leonard.imbert@clever-cloud.com>

--- db/db.sql
+++ db/db.sql
@@ -1,18 +1,19 @@
 create table me_user (
-    me_user_id uuid primary key,
-    username text not null,
-    password text not null,
-    localisation text not null,
-    mail text not null
+  me_user_id uuid primary key,
+  username text not null,
+  active boolean not null,
+  password text not null,
+  localisation text not null,
+  mail text not null
 );
 
 create table me_message (
-    me_message_id uuid primary key,
-    time timestamp with time zone default now() not null,
-    content text not null,
-    me_user_id uuid not null
+  me_message_id uuid primary key,
+  time timestamp with time zone default now() not null,
+  content text not null,
+  me_user_id uuid not null
 );
 
 
-alter table only 
+alter table only
 me_message add constraint me_user_me_user_id_fkey foreign key (me_user_id) references me_user(me_user_id);
